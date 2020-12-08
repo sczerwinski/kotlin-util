5ecf45f71d7f4e7b8f0ff782ce2de916d34c68b4
Slawomir Czerwinski <slawomir@czerwinski.it>
2020-12-08 18:11:58 +0000

    Update Kotlin to v1.4.21

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,7 @@
 ## [Unreleased]
 
 ### Changed
+- Kotlin 1.4.21
 - Gradle Wrapper 6.7.1
 
 ## [v1.4.20]
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.plugin.mpp.AbstractKotlinNativeTargetPreset
 
 plugins {
-    kotlin("multiplatform") version "1.4.20"
+    kotlin("multiplatform") version "1.4.21"
     id("io.gitlab.arturbosch.detekt") version "1.14.2"
     id("org.jetbrains.dokka") version "1.4.10"
     `maven-publish`
