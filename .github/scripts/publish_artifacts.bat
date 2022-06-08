64139bb97eaa9897f5ce3482b1fd71d2ad02964f
Slawomir Czerwinski <slawomir@czerwinski.it>
2022-06-08 21:29:02 +0100

    Fix publishing version with suffix

--- .github/scripts/publish_artifacts.bat
+++ .github/scripts/publish_artifacts.bat
@@ -5,17 +5,21 @@ set SUFFIX=%2
 
 echo Publishing for %BUILD_OS%
 
-if "%BUILD_OS:~0,6%"=="ubuntu" (
+if [%BUILD_OS:~0,6%]==[ubuntu] (
   set TASKS=publishLinuxMips32PublicationToMavenRepository publishLinuxMipsel32PublicationToMavenRepository
-) else if "%BUILD_OS:~0,7%"=="windows" (
+) else if [%BUILD_OS:~0,7%]==[windows] (
   set TASKS=publishMingwX64PublicationToMavenRepository publishMingwX86PublicationToMavenRepository
-) else if "%BUILD_OS:~0,5%"=="macos" (
+) else if [%BUILD_OS:~0,5%]==[macos] (
   set TASKS=publish
 )
 
 echo Running tasks %TASKS%
 echo.
 
-./gradlew %TASKS% "-PversionSuffix=%SUFFIX%" "-Psigning.secretKeyRingFile=secring.gpg" "-Psigning.password=%SIGNING_PASS%" "-Psigning.keyId=%SIGNING_KEY_ID%" --stacktrace
+if [%SUFFIX%] == [] (
+  gradlew %TASKS% "-Psigning.secretKeyRingFile=secring.gpg" "-Psigning.password=%SIGNING_PASS%" "-Psigning.keyId=%SIGNING_KEY_ID%" --stacktrace
+) else (
+  gradlew %TASKS% "-PversionSuffix=%SUFFIX%" "-Psigning.secretKeyRingFile=secring.gpg" "-Psigning.password=%SIGNING_PASS%" "-Psigning.keyId=%SIGNING_KEY_ID%" --stacktrace
+)
 
 echo on
